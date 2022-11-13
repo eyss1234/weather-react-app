261919ca71bdedf11824d7bcaaf1e5cd38b82314
Emily Santos <eyssantos@gmail.com>
2022-11-13 20:17:13 +0000

    temperature change when clicking celsius or fahrenheit

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <h2 className="temperature">{Math.round(props.celsius)}
-    <span className="unit">°C</span></h2> 
-  )
+  const [unit, setUnit] = useState("celsius");
+  
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9/5) + 32 ;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <h2 className="temperature">{Math.round(props.celsius)}
+        <span className="unit">
+        °C | <a href="/" onClick={showFahrenheit}>°F</a>
+        </span></h2>
+      </div>
+    )
+  } else {
+    return (
+    <div className="WeatherTemperature">
+        <h2 className="temperature">{Math.round(fahrenheit())}
+        <span className="unit">
+        <a href="/" onClick={showCelsius}>°C</a> | °F
+        </span></h2>
+    </div>
+    )
+  }
 }
