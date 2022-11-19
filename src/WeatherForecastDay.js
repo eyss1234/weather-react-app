a8f3744191f96de1620f2db037cade7f43138dfa
Emily Santos <eyssantos@gmail.com>
2022-11-19 17:18:57 +0000

    add function to round min and max temp and display day

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,13 +2,30 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    return days[day];
+  }
+
   return (
     <div className="WeatherForecastDay">
-      <div className="forecast-day mb-2"><strong>{props.data[0].dt}</strong></div>
+      <div className="forecast-day mb-2"><strong>{day()}</strong></div>
       <WeatherIcon code={props.data[0].weather[0].icon} size={40}/>
       <div className="forecast-temperatures">
-        <span className="forecast-temperature-max">{props.data[0].temp.max}</span>  
-        <span className="forecast-temperature-min">{props.data[0].temp.min}</span> 
+        <span className="forecast-temperature-max">{maxTemperature()}</span>  
+        <span className="forecast-temperature-min">{minTemperature()}</span> 
       </div>
     </div>
   )
