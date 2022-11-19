389dbedb3b34ee006ae0cc4556484c94d94052c9
Emily Santos <eyssantos@gmail.com>
2022-11-19 18:29:59 +0000

    change color pallet

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
   return (
     <ReactAnimatedWeather 
       icon={codeMapping[props.code]}
-      color="#bb440d"
+      color="#98A8F8"
       size={props.size}
       animate={true}
     />
